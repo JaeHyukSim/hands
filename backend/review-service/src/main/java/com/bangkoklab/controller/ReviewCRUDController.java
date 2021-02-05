1e3f7f4f4cc149ffd5ebac3943ffea9a2feb0005
jaehyukshim <jaehyukawot@gmail.com>
2021-02-05 11:33:25 +0900

    eureka 설정 및 review controller 정리

--- backend/review-service/src/main/java/com/bangkoklab/controller/ReviewCRUDController.java
+++ backend/review-service/src/main/java/com/bangkoklab/controller/ReviewCRUDController.java
@@ -12,9 +12,15 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bangkoklab.data.vo.ImgOfOutput;
+import com.bangkoklab.data.vo.ReviewMass;
 import com.bangkoklab.data.vo.ReviewRequestMessage;
 import com.bangkoklab.data.vo.ReviewResponseMessage;
+import com.bangkoklab.data.vo.ReviewUrlVO;
+import com.bangkoklab.data.vo.ReviewVO;
+import com.bangkoklab.service.ImgService;
 import com.bangkoklab.service.ReviewService;
+import com.bangkoklab.service.UrlService;
 
 @RestController
 @RequestMapping("/review")
@@ -22,6 +28,10 @@ public class ReviewCRUDController {
 	
 	@Autowired
 	ReviewService reviewService;
+	@Autowired
+	ImgService imgService;
+	@Autowired
+	UrlService urlService;
 	
 	@GetMapping("/get-reviews")
 	public List<ReviewResponseMessage> getReviews(ReviewRequestMessage reviewRequestMessage) {
@@ -30,9 +40,63 @@ public class ReviewCRUDController {
 	
 	/**
 	 * review를 insert하는 컨트롤러입니다
+	 * ****************************
+	 * review를 insert하는 것은 일단 swagger2에서는 안되고
+	 * static에 있는 index.html을 이용합니다.
+	 * ---------------------------------------------
+	 * 다음에 swagger에서 파일 업로드 되도록 구현해보기
 	 */
 	@PostMapping("/insert-review")
 	public void insertReview(ReviewRequestMessage reviewRequestMessage) {
 		reviewService.insertReviewProcess(reviewRequestMessage);
+	}
+	/**
+	 * 리뷰 리스트를 user_uuid로 가져옵니다
+	 * @param reviewRequestMessage
+	 * @return
+	 */
+	@GetMapping("/get-review-list")
+	public List<ReviewVO> getReviewList(ReviewRequestMessage reviewRequestMessage) {
+		return reviewService.selectReview(reviewRequestMessage.getUserUuid());
+	}
+	/**
+	 * 리뷰 리스트를 target uuid로 가져옵니다
+	 * @param targetUuid
+	 * @return
+	 */
+	@GetMapping("/get-review-list-by-target")
+	public List<ReviewVO> getReviewListByTarget(String targetUuid) {
+		return reviewService.getReviewByTargetUuid(targetUuid);
+	}
+	/**
+	 * 모든 리뷰를 가져옵니다
+	 */
+	@GetMapping("/get-all-review")
+	public List<ReviewVO> getAllReview() {
+		return reviewService.getAllReview();
+	}
+
+	/**
+	 * 모든 이미지를 가져옵니다
+	 */
+	@PostMapping("/get-img-by-review-id")
+	public List<ImgOfOutput> getImgByReviewId(String reviewId) {
+		return imgService.getImgByReviewId(reviewId);
+	}
+
+	/**
+	 * 모든 url을 가져옵니다
+	 */
+	@GetMapping("/get-url-by-review-id")
+	public List<ReviewUrlVO> getUrlByReviewId(String reviewId) {
+		return urlService.getUrlsByReviewId(reviewId);
+	}
+
+	/**
+	 * review와 imgs, urls를 모두 가져옵니다
+	 */
+	@GetMapping("get-mass-review")
+	public List<ReviewMass> getMassReview() {
+		return reviewService.getAllReviewWithImgAndUrl();
 	}
 }
