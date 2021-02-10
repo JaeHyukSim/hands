852a800542813d96d6f211148a215954a03882d0
winmo95 <seungmo6722@gmail.com>
2021-02-10 10:52:48 +0900

    fix/get json -> pathVariable

--- backend/FollowHandy/src/main/java/com/bangkoklab/FollowHandy/service/FollowService.java
+++ backend/FollowHandy/src/main/java/com/bangkoklab/FollowHandy/service/FollowService.java
@@ -6,7 +6,7 @@ import com.bangkoklab.FollowHandy.dto.Handy;
 
 public interface FollowService {
 	public void FollowHandy(Handy handy) throws Exception;
-	List<Handy> FindFollowHandy(Handy handy) throws Exception;
+	List<Handy> FindFollowHandy(String MyId) throws Exception;
 	void deleteFollowById(Handy handy) throws Exception;
 	boolean FindFollowById(Handy handy) throws Exception;
 }
